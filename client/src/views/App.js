b3ab70957d2381408674c57f9b9323e6a838a5c7
Tim <besttimothy1@gmail.com>
2016-07-10 14:10:33 -0700

    almost working version with summary tag

--- client/src/views/App.js
+++ client/src/views/App.js
@@ -3,45 +3,74 @@ import React from 'react';
 
 class ItemControls extends React.Component {
 
+  handleClick(event, action) {
+    this.props.onAllItemDisplay(action);
+  }
+
   render() {
     return (
       <div className="item-controls">
         <div>
-          <button>Open All</button>
-          <button>Close All</button>
-          <button>Toggle All</button>
+          <button onClick={(event) => this.handleClick(event, "open")}>Open All</button>
+          <button onClick={(event) => this.handleClick(event, "close")}>Close All</button>
+          <button onClick={(event) => this.handleClick(event, "toggle")}>Toggle All</button>
         </div>
         <button>Add</button>
       </div>
     );
   }
-
 }
 
 class Item extends React.Component {
 
+  handleClick() {
+    this.props.toggleItem(this.props.index);
+  }
+
   render() {
     return (
-      <details>
-        <summary>{this.props.title}</summary>
+      <details index={this.props.index} open={open}>
+        <summary onClickCapture={(event) => this.handleClick(event)}>{this.props.title}</summary>
         <p>{this.props.content}</p>
       </details>
     );
   }
-
 }
 
 class ItemList extends React.Component {
 
+  toggleItem(index) {
+    this.props.toggleItem(index);
+  }
+
   render() {
     var rows = [];
-    var searchTerm = this.props.searchTerm;
+    var itemList = this;
 
-    this.props.items.forEach(function(item) {
-      if (item.title.indexOf(searchTerm) === -1 && item.content.indexOf(searchTerm) === -1) {
+    this.props.items.forEach(function(item, key) {
+      if (item.title.indexOf(itemList.props.searchTerm) === -1 && item.content.indexOf(itemList.props.searchTerm) === -1) {
         return;
       }
-      rows.push(<Item title={item.title} content={item.content} />);
+
+      if (itemList.props.display == "open") {
+        item.open = true;
+      }
+      else if (itemList.props.display == "close") {
+        item.open = false;
+      }
+      else if (itemList.props.display == "toggle") {
+        item.open = !item.open;
+      }
+      rows.push(
+        <Item
+          key={key}
+          index={key}
+          title={item.title}
+          content={item.content}
+          open={item.open}
+          toggleItem={(index) => itemList.toggleItem(index)}
+        />
+      );
     });
     return (
       <div className="item-area">
@@ -49,13 +78,12 @@ class ItemList extends React.Component {
       </div>
     );
   }
-
 }
 
 class SearchBar extends React.Component {
 
   handleChange(event) {
-    this.props.onUserInput(
+    this.props.onUserSearch(
       this.refs.searchTerm.value
     );
   }
@@ -65,10 +93,9 @@ class SearchBar extends React.Component {
       placeholder="Search"
       value={this.props.searchTerm}
       ref="searchTerm"
-      onChange={(e) => this.handleChange(e)}
+      onChange={(event) => this.handleChange(event)}
     />;
   }
-
 }
 
 class Header extends React.Component {
@@ -76,7 +103,6 @@ class Header extends React.Component {
   render() {
     return <h1 className="text-center">{this.props.title}</h1>;
   }
-
 }
 
 class ItemListForm extends React.Component {
@@ -85,30 +111,49 @@ class ItemListForm extends React.Component {
     super(props);
     this.state = {
       searchTerm: "",
+      display: "",
+      items: [
+        {title: "Title1", content: "content1", open: false},
+        {title: "Title2", content: "content2", open: true}
+      ]
     };
   }
 
-  handleUserInput(searchTerm) {
+  handleUserSearch(searchTerm) {
+    this.setState({searchTerm: searchTerm});
+  }
+
+  handleAllItemDisplay(action) {
+    this.setState({display: action});
+  }
+
+  toggleItem(index) {
+    var items = this.state.items;
+
+    items[index].open = !items[index].open;
     this.setState({
-      searchTerm: searchTerm
+       items: items
     });
   }
 
   render() {
     return (
       <div>
-        <SearchBar searchTerm={this.state.searchTerm} onUserInput={(e) => this.handleUserInput(e)}/>
-        <ItemList items={ITEMS} searchTerm={this.state.searchTerm}/>
-        <ItemControls searchTerm={this.state.searchTerm}/>
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          onUserSearch={(searchTerm) => this.handleUserSearch(searchTerm)}
+        />
+        <ItemList
+          items={this.state.items}
+          searchTerm={this.state.searchTerm}
+          display={this.state.display}
+          toggleItem={(key) => this.toggleItem(key)}
+        />
+        <ItemControls onAllItemDisplay={(action) => this.handleAllItemDisplay(action)}/>
       </div>
     );
   }
 }
-
-var ITEMS = [
-  {title: "Title1", content: "content1"},
-  {title: "Title2", content: "content2"}
-];
 
 export default class App extends React.Component {
 
@@ -116,9 +161,8 @@ export default class App extends React.Component {
     return (
       <div className="verbling-challenge">
         <Header title="Verbling Challenge"/>
-        <ItemListForm items={ITEMS}/>
+        <ItemListForm/>
       </div>
     );
   }
-
 }
